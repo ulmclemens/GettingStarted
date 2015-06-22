fc51ae9cb03577a8288b02309361e6d11d4f0e17
Yigal Duppen <yigal@publysher.nl>
2015-06-22 12:46:54 +0200

    Don't configure stuff we don't use

--- src/com/company/GettingStartedConfiguration.java
+++ src/com/company/GettingStartedConfiguration.java
@@ -1,32 +1,7 @@
 package com.company;
 
 import io.dropwizard.Configuration;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import org.hibernate.validator.constraints.NotEmpty;
 
 public class GettingStartedConfiguration extends Configuration {
-    @NotEmpty
-    String template;
-    @NotEmpty
-    String defaultName = "Stranger";
 
-    @JsonProperty
-    public String getTemplate() {
-        return template;
-    }
-
-    @JsonProperty
-    public void setTemplate(String newTemplate) {
-        template = newTemplate;
-    }
-
-    @JsonProperty
-    public String getDefaultName() {
-        return defaultName;
-    }
-
-    @JsonProperty
-    public void setDefaultName(String name) {
-        this.defaultName = name;
-    }
 }
